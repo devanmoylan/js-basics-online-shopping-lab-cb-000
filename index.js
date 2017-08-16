454cdaedd4af02d673399e4b677dc836c0dd9e81
Devan <devan.moylan@backyardlion.com>
2017-08-15 21:14:49 -0600

    Added a funtion to addToItemToCart.

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 var cart = [];
-
+var item = "shoe";
 function getCart() {
- return cart;
+    return cart;
 }
 
 function setCart(c) {
-  cart = c;
-  return cart;
+    cart = c;
+    return cart;
 }
 
 function addToCart(item) {
- // write your code here
+    var price = Math.floor(Math.random() * (100 - 1) + 1);
+    cart.push({[item]: price});
+    console.log(`${item} has been added to your cart.`);
+    return cart;
 }
 
+
 function viewCart() {
-  // write your code here
+    if (cart.length === 0) {
+        return 'Your shopping cart is empty.'
+    } else {
+        var cartList = []
+        for (let i = 0; i < cart.length; i++) {
+          cartList.push(`${Object.keys(cart)[i]} at ${cart[i]}`)
+        }
+    }
 }
 
 function total() {
-  // write your code here
+    // write your code here
 }
 
 function removeFromCart(item) {
-  // write your code here
+    // write your code here
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+    // write your code here
 }
